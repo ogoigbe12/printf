ae8364ac9da97c574964e1a387d57833607051d1
samuel ogoigbe <ogoigbe12@gmail.com>
2021-07-23 07:22:52 -0700

    new file

---
+++ holberton.h
@@ -0,0 +1,23 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/* _printf */
+int _printf(const char *format, ...);
+#endif
